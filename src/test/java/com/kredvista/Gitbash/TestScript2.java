6933daf4bdc263c4b9528789a1ab79e6c99c6c6d
ashirbad121 <ashirbad.sonu123@gmail.com>
2023-11-28 18:07:04 +0530

    commited

---
+++ src/test/java/com/kredvista/Gitbash/TestScript2.java
@@ -0,0 +1,18 @@
+package com.kredvista.Gitbash;
+
+import org.testng.annotations.Test;
+
+public class TestScript2 {
+
+	@Test
+	public void testscript2()
+	{
+		System.out.println("--ts2---");
+	}
+	
+	@Test
+	public void testscript21()
+	{
+		System.out.println("--ts21---");
+	}
+}
