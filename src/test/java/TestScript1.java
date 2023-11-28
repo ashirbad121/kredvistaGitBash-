374e87475fdb35c6ea3882a0cd4f9a5d2efc0563
ashirbad121 <ashirbad.sonu123@gmail.com>
2023-11-28 18:20:15 +0530

    commited

---
+++ src/test/java/TestScript1.java
@@ -0,0 +1,18 @@
+
+
+import org.testng.annotations.Test;
+
+public class TestScript1 {
+
+	@Test
+	public void testscript1()
+	{
+		System.out.println("--ts1---");
+	}
+	
+	@Test
+	public void testscript12()
+	{
+		System.out.println("--ts12---");
+	}
+}
